e96b741c7a59d0b0ee8460cc4adbb5ff094425b5
Ian Gilham <ian.gilham@gmail.com>
2020-12-16 16:53:52 +0000

    problem 6 part 2 done

--- advent06/run.go
+++ advent06/run.go
@@ -6,21 +6,43 @@ import "fmt"
 func Run(groupedLines [][]string) {
 	// each line in a group is a person
 	// each char in a line is a question they answer "yes" to
-	total := 0
+	totalAny := 0
+	totalEvery := 0
 	for i, group := range groupedLines {
-		n := countAnswers(group)
-		total = total + n
-		fmt.Printf("group %v: %v common answers\n", i, n)
+		n, m := countAnswers(group)
+		totalAny = totalAny + n
+		totalEvery = totalEvery + m
+		fmt.Printf("group %v: anyone=%v,  everyone=%v\n", i, n, m)
 	}
-	fmt.Printf("total: %v\n", total)
+	fmt.Printf("total for which anyone answered 'yes': %v\n", totalAny)
+	fmt.Printf("total for which everyone answered 'yes': %v\n", totalEvery)
 }
 
-func countAnswers(group []string) int {
-	m := make(map[rune]bool, 26)
+func loadAnswers(group []string) map[rune]int {
+	m := make(map[rune]int, 26)
 	for _, line := range group {
 		for _, b := range line {
-			m[b] = true
+			n := m[b]
+			if n == 0 {
+				m[b] = 1
+			} else {
+				m[b] = n + 1
+			}
 		}
 	}
-	return len(m)
+	return m
+}
+
+func countAnswers(group []string) (anyone int, everyone int) {
+	m := loadAnswers(group)
+
+	nPeople := len(group)
+	anyone = len(m)
+	everyone = 0
+	for _, n := range m {
+		if n == nPeople {
+			everyone++
+		}
+	}
+	return
 }
