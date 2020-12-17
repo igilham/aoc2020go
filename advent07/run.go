88c8eacc2e19e26a2baa98bdcbd1735b5121e010
Ian Gilham <ian.gilham@gmail.com>
2020-12-17 09:58:29 +0000

    parse problem 7 input

--- advent07/run.go
+++ advent07/run.go
@@ -1,65 +1,81 @@
 package advent07
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
-	"text/scanner"
-
-	"github.com/igilham/aoc2020go/util"
 )
 
 var isNumberRegexp = regexp.MustCompile("^\\d+$")
 
 // Run runs the seventh problem
 func Run(lines []string) {
-	bags := parseBagTypes(lines)
-	util.PrintLines(bags)
+	bags := parseRules(lines)
+
+	fmt.Printf("Rules:\n%v", bags)
 	fmt.Printf("total: %v", len(bags))
 }
 
-// pull out all types of bags, including duplicates
-// TODO: remove duplicates
-// TODO: encode container rules and numbers in response
-func parseBagTypes(lines []string) []string {
-	bags := []string{}
+type BagRules map[string]Contents
+
+func (r BagRules) String() string {
+	var sb strings.Builder
+	for k, v := range r {
+		contents := []string{}
+		for k1, v1 := range v {
+			contents = append(contents, fmt.Sprintf("%v %v", v1, k1))
+		}
+		contentsStr := strings.Join(contents, ", ")
+		if contentsStr == "" {
+			contentsStr = "no other bags."
+		}
+
+		sb.WriteString(fmt.Sprintf("%v bags contain %v.\n", k, contentsStr))
+	}
+	return sb.String()
+}
+
+type Contents map[string]int
+
+func parseRules(lines []string) BagRules {
+	bags := make(BagRules)
 	for _, line := range lines {
-		var s scanner.Scanner
-		s.Init(strings.NewReader(line))
-		w1 := ""
-		for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
-			t := s.TokenText()
-			switch t {
-			case "no":
-				fallthrough
-			case "other":
-				fallthrough
-			case "bag":
-				fallthrough
-			case "bags":
-				fallthrough
-			case "contain":
-				fallthrough
-			case ",":
-				continue
-			default:
-				isNumber := isNum(t)
-				if isNumber {
-					continue
-				}
-				if w1 == "" {
-					w1 = t
-					continue
-				} else {
-					bags = append(bags, fmt.Sprintf("%s %s", w1, t))
-					w1 = ""
-				}
-			}
+		topParts := strings.Split(line, " bags contain ")
+		s := topParts[0]
+		if bags[s] == nil {
+			bags[s] = parseContents(topParts[1])
 		}
 	}
 	return bags
 }
 
-func isNum(s string) bool {
-	return isNumberRegexp.MatchString(s)
+func parseContents(s string) Contents {
+	contents := make(Contents)
+
+	containedStr := strings.TrimSuffix(s, ".")
+	contained := strings.Split(containedStr, ", ")
+	for _, b := range contained {
+		words := strings.Split(b, " ")
+		count, err := getNum(words[0])
+		if err == nil {
+			containedBag := strings.Join(words[1:len(words)-1], " ")
+			contents[containedBag] = count
+		}
+	}
+
+	return contents
+}
+
+func getNum(s string) (int, error) {
+	m := isNumberRegexp.FindString(s)
+	if m == "" {
+		return 0, errors.New("no number found")
+	}
+	i, err2 := strconv.Atoi(m)
+	if err2 != nil {
+		return 0, err2
+	}
+	return i, nil
 }
