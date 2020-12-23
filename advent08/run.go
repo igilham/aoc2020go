5b564a6e8f3c3b017091815737a5e6d1af5f351d
Ian Gilham <ian.gilham@gmail.com>
2020-12-23 19:46:29 +0000

    problem 8 done

--- advent08/run.go
+++ advent08/run.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+var nullInstruction = Instruction{
+	op:  "nop",
+	arg: 0,
+}
+
 // Run runs the eighth problem
 func Run(lines []string) {
 	program, err := parseInstructions(lines)
@@ -15,14 +20,44 @@ func Run(lines []string) {
 	}
 
 	problem1(program)
+	problem2(program)
 }
 
-func problem1(program []*Instruction) {
+func problem1(program Programme) {
 	cpu := newCPU()
 	err := cpu.execProgram(program)
 	if err != nil {
-		fmt.Println("infinite loop found")
 		fmt.Println(err)
+	}
+	fmt.Printf("program terminated in state: %v\n", cpu)
+}
+
+func problem2(program Programme) {
+	fmt.Println("searching for a terminating programme")
+	cpu := newCPU()
+SEARCHLOOP:
+	for i, instruction := range program {
+		cpu.reset()
+		switch instruction.op {
+		case "nop":
+			program[i].op = "jmp"
+			nerr := cpu.execProgram(program)
+			if nerr == nil {
+				fmt.Printf("terminates when changing %v from nop to jmp\n", i)
+				break SEARCHLOOP
+			} else {
+				program[i].op = "nop" // undo change
+			}
+		case "jmp":
+			program[i].op = "nop"
+			nerr := cpu.execProgram(program)
+			if nerr == nil {
+				fmt.Printf("terminates when changing %v from jmp to nop\n", i)
+				break SEARCHLOOP
+			} else {
+				program[i].op = "jmp" // undo change
+			}
+		}
 	}
 	fmt.Printf("program terminated in state: %v\n", cpu)
 }
@@ -44,7 +79,12 @@ func (cpu *CPU) String() string {
 	return fmt.Sprintf("pc=%v, acc=%v", cpu.pc, cpu.acc)
 }
 
-func (cpu *CPU) exec(i *Instruction) {
+func (cpu *CPU) reset() {
+	cpu.pc = 0
+	cpu.acc = 0
+}
+
+func (cpu *CPU) exec(i Instruction) {
 	switch i.op {
 	case "nop":
 		cpu.pc++
@@ -56,13 +96,13 @@ func (cpu *CPU) exec(i *Instruction) {
 	}
 }
 
-func (cpu *CPU) execProgram(program []*Instruction) error {
+func (cpu *CPU) execProgram(program Programme) error {
 	executed := make(map[int]bool)
 	for cpu.pc < len(program) {
 		currentPC := cpu.pc
 		i := program[currentPC]
 		if found := executed[currentPC]; found == true {
-			return fmt.Errorf("already executed %v: \"%v\", (%v)", currentPC, i, cpu)
+			return fmt.Errorf("infinite loop at pc %v: \"%v\", (%v)", currentPC, i, cpu)
 		}
 		// fmt.Printf("executing %v: %v\n", currentPC, i)
 		cpu.exec(i)
@@ -71,6 +111,9 @@ func (cpu *CPU) execProgram(program []*Instruction) error {
 	}
 	return nil
 }
+
+// Programme is a slice of instructions
+type Programme []Instruction
 
 // Instruction represents a CPU instruction
 type Instruction struct {
@@ -82,8 +125,8 @@ func (i *Instruction) String() string {
 	return fmt.Sprintf("%v %v", i.op, i.arg)
 }
 
-func parseInstructions(lines []string) ([]*Instruction, error) {
-	instructions := []*Instruction{}
+func parseInstructions(lines []string) (Programme, error) {
+	instructions := Programme{}
 	for _, s := range lines {
 		i, err := parseInstruction(s)
 		if err != nil {
@@ -94,14 +137,14 @@ func parseInstructions(lines []string) ([]*Instruction, error) {
 	return instructions, nil
 }
 
-func parseInstruction(s string) (*Instruction, error) {
+func parseInstruction(s string) (Instruction, error) {
 	parts := strings.Split(s, " ")
 	arg, err := strconv.Atoi(parts[1])
 	if err != nil {
-		return nil, err
+		return nullInstruction, err
 	}
 
-	return &Instruction{
+	return Instruction{
 		op:  parts[0],
 		arg: arg,
 	}, nil
